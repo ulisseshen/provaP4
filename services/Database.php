ecc6ef9ba99f7abb63108586cb20617fb5a2313f
Ulisses Hen <ulisses@hens.com.br>
2023-11-27 10:01:12 -0300

    refactor: database connection code

--- services/Database.php
+++ services/Database.php
@@ -3,10 +3,18 @@ class Database
 {
     private $pdo;
 
-    public function __construct($host, $dbname, $user, $password)
+    public function __construct()
     {
-        $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $host = "localhost";
+        $dbname = "provaP4";
+        $user = "root";
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $user);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexão bem-sucedida";
+        } catch (PDOException $e) {
+            echo "Conexão falhou: " . $e->getMessage();
+        }
     }
 
     public function getPdo()
