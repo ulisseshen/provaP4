5b0f89f3cb32bd57a19b3d9f853e11d2941efe14
Ulisses Hen <ulisses@hens.com.br>
2023-11-27 10:00:38 -0300

    chore: add user registration functionality

--- Register.php
+++ Register.php
@@ -1,12 +1,23 @@
 <?php 
+    require_once __DIR__ . "/models/User.php";
+    require_once __DIR__ . "/repositories/UserRepository.php";
+    require_once __DIR__ . "/services/Database.php";
     $name = $_POST['name'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $user = new User($name, $lastName, $email, $password);
-    $db = 
+    $db = new Database();
     $userRepository = new UserRepository($db);
-    $userRepository->save($user);
+    $result = $userRepository->saveNew($user);
+    if ($result->isSuccess()) {
+        header("Location: /login.php");
+    } else {
+        //retornar status code 
+        http_response_code($result->getStatus());
+        echo $result->getJson();
+
+    }
 
 ?>
